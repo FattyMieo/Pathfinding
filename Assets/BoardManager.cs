9e2757cd0356f39d7383fab9340cfd26a2f441bf
FattyMieo <fong2009@live.com.my>
2018-02-21 17:35:42 +0800

    Transfer to GitHub

---
+++ Assets/BoardManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+	public GameObject tilePrefab;
+
+	public float tileSize = 1.0f;
+
+	private float tileWidth;
+	private float tileHeight;
+
+	private float spacingWidth;
+	private float spacingHeight;
+
+	public int boardSize = 5;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Init();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void Init()
+	{
+		tileWidth = tileSize;
+		tileHeight = MathExtension.sqrt3 / 2.0f * tileWidth;
+
+		spacingWidth = tileWidth * 3.0f / 4.0f;
+		spacingHeight = tileHeight;
+	}
+
+	public void GetNeighbours(int x, int y, int direction)
+	{
+		axialDirections[direction]
+	}
+
+	public void CreateBoard()
+	{
+		for(int i = 0; i < boardSize; i++)
+		{
+			for(int j = 0; j < i * 6; j++)
+			{
+
+			}
+		}
+	}
+}
---
+++ Assets/Hex.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex
+{
+	private static Vector2Int[] axialDirections =
+	{
+		Vector2Int.down, Vector2Int.down + Vector2Int.right, Vector2Int.right,
+		Vector2Int.up, Vector2Int.up + Vector2Int.left, Vector2Int.left
+	};
+
+
+
+	public int x;
+	public int y;
+}
